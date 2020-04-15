18af61955977f7b09ad9b23f3f2a348b57777d69
nialov <nikolasovaskainen@hotmail.com>
2020-04-15 19:37:35 +0300

    Added log messages and lessened load on memory by deleting unneeded columns.

--- fracture_analysis_kit/analysis_and_plotting.py
+++ fracture_analysis_kit/analysis_and_plotting.py
@@ -2,7 +2,7 @@
 Handles analysis and plotting using a MultiTargetAreaAnalysis class. Analysis (i.e. heavy calculations)
 and plotting have been separated into two different methods.
 """
-
+from qgis.core import QgsMessageLog, Qgis
 import config
 from fracture_analysis_kit import qgis_tools as qgis_tools, multiple_target_areas as mta
 
@@ -35,10 +35,19 @@ class MultiTargetAreaAnalysis:
         self.analysis_branches = None
 
         # Convert QGIS-layers to GeoDataFrames
+        # and drop unused columns to free memory
         self.layer_table_df['Trace_frame'] = self.layer_table_df['Trace'].apply(qgis_tools.layer_to_gdf)
+        self.layer_table_df = self.layer_table_df.drop("Trace", 1)
+
         self.layer_table_df['Branch_frame'] = self.layer_table_df['Branch'].apply(qgis_tools.layer_to_gdf)
+        self.layer_table_df = self.layer_table_df.drop("Branch", 1)
+
         self.layer_table_df['Area_frame'] = self.layer_table_df['Area'].apply(qgis_tools.layer_to_gdf)
+        self.layer_table_df = self.layer_table_df.drop("Area", 1)
+
         self.layer_table_df['Node_frame'] = self.layer_table_df['Node'].apply(qgis_tools.layer_to_gdf)
+        self.layer_table_df = self.layer_table_df.drop("Node", 1)
+
 
         # Check if cross-cutting and abutting relationships can be determined
         if len(self.set_df) < 2:
@@ -53,17 +62,28 @@ class MultiTargetAreaAnalysis:
         # DEBUG
         # self.layer_table_df = self.layer_table_df.drop(columns=['Trace', 'Branch', 'Area', 'Node'])
 
+        QgsMessageLog.logMessage(
+            message="Initializing MultiTargetAreaQGIS objects for traces and branches."
+            , tag=f'{__name__}', level=Qgis.Info)
+
         self.analysis_traces = mta.MultiTargetAreaQGIS(self.layer_table_df, self.group_names_cutoffs_df, branches=False)
         # Bar at 25
 
         self.analysis_branches = mta.MultiTargetAreaQGIS(self.layer_table_df, self.group_names_cutoffs_df,
                                                          branches=True)
         # Bar at 45
-
+        QgsMessageLog.logMessage(
+            message="Calculating attributes for traces.", tag=f'{__name__}', level=Qgis.Info)
         # TRACE DATA SETUP
         self.analysis_traces.calc_attributes_for_all()
 
+        QgsMessageLog.logMessage(
+            message="Defining sets for traces.", tag=f'{__name__}', level=Qgis.Info)
+
         self.analysis_traces.define_sets_for_all(self.set_df)
+
+        QgsMessageLog.logMessage(
+            message="Defining grouping for traces.", tag=f'{__name__}', level=Qgis.Info)
 
         # self.analysis_traces.calc_curviness_for_all()
         self.analysis_traces.unified()
@@ -71,23 +91,44 @@ class MultiTargetAreaAnalysis:
         # TODO: Check setframes
         # self.analysis_traces.create_setframes_for_all_unified()
 
+        QgsMessageLog.logMessage(
+            message="Gathering topological parameters for traces."
+            , tag=f'{__name__}', level=Qgis.Info)
+
         self.analysis_traces.gather_topology_parameters(unified=False)
         self.analysis_traces.gather_topology_parameters(unified=True)
 
         # Cross-cutting and abutting relationships
+        QgsMessageLog.logMessage(
+            message="Determining cross-cutting and abutting relationships for traces."
+            , tag=f'{__name__}', level=Qgis.Info)
         if self.determine_relationships:
             self.analysis_traces.determine_crosscut_abutting_relationships(unified=False)
             self.analysis_traces.determine_crosscut_abutting_relationships(unified=True)
 
         # BRANCH DATA SETUP
+        QgsMessageLog.logMessage(
+            message="Calculating attributes for branches.", tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.calc_attributes_for_all()
 
+        QgsMessageLog.logMessage(
+            message="Defining sets for branches.", tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.define_sets_for_all(self.set_df)
 
+        QgsMessageLog.logMessage(
+            message="Defining grouping for branches.", tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.unified()
+
+        QgsMessageLog.logMessage(
+            message="Gathering topological parameters for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
 
         self.analysis_branches.gather_topology_parameters(unified=False)
         self.analysis_branches.gather_topology_parameters(unified=True)
+
+        QgsMessageLog.logMessage(
+            message="Defining anisotropy of connectivity for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
 
         # Anisotropy
         self.analysis_branches.calc_anisotropy(unified=False)
@@ -100,6 +141,9 @@ class MultiTargetAreaAnalysis:
         # ___________________BRANCH DATA_______________________
         config.styling_plots("branches")
         # Length distributions
+        QgsMessageLog.logMessage(
+            message="Plotting branch length distributions."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_lengths(unified=False, save=True,
                                             savefolder=self.plotting_directory + '/length_distributions/indiv/branches')
         self.analysis_branches.plot_lengths(unified=True, save=True,
@@ -111,23 +155,34 @@ class MultiTargetAreaAnalysis:
         #         save=True, savefolder=self.plotting_directory + '/length_distributions/branches/predictions', predict_with=p)
 
         # Azimuths
+        QgsMessageLog.logMessage(
+            message="Plotting azimuths for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_azimuths_weighted(unified=False, save=True,
                                                       savefolder=self.plotting_directory + '/azimuths/indiv')
         self.analysis_branches.plot_azimuths_weighted(unified=True, save=True,
                                                       savefolder=self.plotting_directory + '/azimuths')
         # XYI
-
+        QgsMessageLog.logMessage(
+            message="Plotting XYI for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_xyi_ternary(unified=False, save=True,
                                                 savefolder=self.plotting_directory + "/xyi")
         self.analysis_branches.plot_xyi_ternary(unified=True, save=True,
                                                 savefolder=self.plotting_directory + "/xyi")
 
         # Topo parameters
+        QgsMessageLog.logMessage(
+            message="Plotting topology for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_topology(unified=False, save=True,
                                              savefolder=self.plotting_directory + '/topology/branches')
         self.analysis_branches.plot_topology(unified=True, save=True,
                                              savefolder=self.plotting_directory + '/topology/branches')
         # Hexbinplots
+        QgsMessageLog.logMessage(
+            message="Plotting hexbinplots for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_hexbin_plot(unified=False, save=True,
                                                 savefolder=self.plotting_directory + '/hexbinplots')
         self.analysis_branches.plot_hexbin_plot(unified=True, save=True,
@@ -135,6 +190,9 @@ class MultiTargetAreaAnalysis:
 
         # ----------------unique for branches-------------------
         # Branch Classification ternary plot
+        QgsMessageLog.logMessage(
+            message="Plotting branch classification plot for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_branch_ternary(unified=False,
                                                    save=True, savefolder=self.plotting_directory + "/branch_class"
                                                    )
@@ -143,6 +201,9 @@ class MultiTargetAreaAnalysis:
                                                    )
 
         # Anisotropy
+        QgsMessageLog.logMessage(
+            message="Plotting anisotropy of connectivity for branches."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_branches.plot_anisotropy(unified=False, save=True,
                                                savefolder=self.plotting_directory + '/anisotropy/indiv')
         self.analysis_branches.plot_anisotropy(unified=True, save=True,
@@ -150,7 +211,9 @@ class MultiTargetAreaAnalysis:
 
         # __________________TRACE DATA______________________
         config.styling_plots('traces')
-
+        QgsMessageLog.logMessage(
+            message="Plotting trace length distributions."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_traces.plot_lengths(unified=False, save=True,
                                           savefolder=self.plotting_directory + '/length_distributions/indiv/traces')
         self.analysis_traces.plot_lengths(unified=True, save=True,
@@ -162,6 +225,9 @@ class MultiTargetAreaAnalysis:
         #         save=True, savefolder=self.plotting_directory + '/length_distributions/traces/predictions', predict_with=p)
 
         # Azimuths
+        QgsMessageLog.logMessage(
+            message="Plotting azimuths for traces."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_traces.plot_azimuths_weighted(unified=False, save=True,
                                                     savefolder=self.plotting_directory + '/azimuths/indiv')
         self.analysis_traces.plot_azimuths_weighted(unified=True, save=True,
@@ -175,11 +241,17 @@ class MultiTargetAreaAnalysis:
         # self.analysis_traces.plot_azimuths(unified=True, rose_type='equal-area', save=True
         #                                    , savefolder=self.plotting_directory + '/azimuths/equal_area/traces')
         # Topo parameters
+        QgsMessageLog.logMessage(
+            message="Plotting topology for traces."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_traces.plot_topology(unified=False, save=True,
                                            savefolder=self.plotting_directory + '/topology/traces')
         self.analysis_traces.plot_topology(unified=True, save=True,
                                            savefolder=self.plotting_directory + '/topology/traces')
         # Hexbinplots
+        QgsMessageLog.logMessage(
+            message="Plotting hexbinplots for traces."
+            , tag=f'{__name__}', level=Qgis.Info)
         self.analysis_traces.plot_hexbin_plot(unified=False, save=True,
                                               savefolder=self.plotting_directory + '/hexbinplots')
         self.analysis_traces.plot_hexbin_plot(unified=True, save=True,
@@ -192,6 +264,9 @@ class MultiTargetAreaAnalysis:
         # Fracture overprinting history using Markov chain analysis:
         # Windsor-Kennetcook subbasin, Maritimes Basin, Canada
         if self.determine_relationships:
+            QgsMessageLog.logMessage(
+                message="Plotting cross-cutting and abutting relationships for traces."
+                , tag=f'{__name__}', level=Qgis.Info)
             self.analysis_traces.plot_crosscut_abutting_relationships(unified=False, save=True,
                                                                       savefolder=self.plotting_directory + '/age_relations/indiv')
             self.analysis_traces.plot_crosscut_abutting_relationships(unified=True, save=True,
