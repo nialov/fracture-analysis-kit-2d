a907e8b5d15ab4b985219eb86b8da956c92a0d98
nialov <nikolasovaskainen@hotmail.com>
2020-04-16 23:58:35 +0300

    Added important checkers for faulty geometries and logging to QGIS.

--- fracture_analysis_kit/qgis_tools.py
+++ fracture_analysis_kit/qgis_tools.py
@@ -4,12 +4,11 @@ Contains tools relevant to QGIS layer transformations and plugin functionality.
 
 import os
 from pathlib import Path
-
+import shapely
 import geopandas as gpd
 import pandas as pd
 from qgis.PyQt.QtWidgets import QMessageBox
-from qgis.core import QgsMessageLog, Qgis
-
+from qgis.core import QgsMessageLog, Qgis, QgsProject
 
 # Convert QGIS vectorlayer to pandas DataFrame
 def layer_to_df(layer):
@@ -19,23 +18,52 @@ def layer_to_df(layer):
     :param layer: QGIS vector layer.
     :type layer: qgis.core.QgsVectorLayer
     :return: Converted pandas DataFrame and layer coord system.
-    :rtypes: pandas.DataFrame & qgis.core.QgsCoordinateReferenceSystem
+    :rtypes: pd.DataFrame | qgis.core.QgsCoordinateReferenceSystem
     """
+    # QgsMessageLog.logMessage(message=f"Converting QgsVectorLayer ({layer.name()}) to DataFrame"
+    #                          , tag=f'{__name__}', level=Qgis.Info)
     fieldnames = [field.name() for field in layer.fields()]
+    # QgsMessageLog.logMessage(message=f"Fieldnames: ({fieldnames})"
+    #                          , tag=f'{__name__}', level=Qgis.Info)
     fieldnames.append('geometry')
+    # features is an iterator. Be wary of exhausting it early.
     features = layer.getFeatures()
-    coord_system = layer.crs()
+    coord_system = QgsProject.instance().crs()
+    if len(coord_system.toProj()) == 0:
+        coord_system = layer.crs()
+        if len(coord_system.toProj()) == 0:
+            raise Exception("Coord system invalid.\n"
+                            f"layer coord_system={str(coord_system.toProj())}"
+                            f"proj coord_system={str(QgsProject.instance().crs().toProj())}")
+    # QgsMessageLog.logMessage(message=f"coord_system: ({str(coord_system)})"
+    #                          , tag=f'{__name__}', level=Qgis.Info)
     df = pd.DataFrame(columns=fieldnames)
-    for feature in features:
+    invalid_count = 0
+    for idx, feature in enumerate(features):
+
         row_add = {}
+        do_not_add = False
         for fn in fieldnames:
             if 'geometry' in fn:
+                geom = feature.geometry()
+                # Find invalid geometries and skip features with them.
+                if geom.isEmpty() or geom.isNull() or not geom.isGeosValid():
+                    do_not_add = True
+                    invalid_count += 1
+                    break
                 row_add[fn] = feature.geometry()
             else:
                 row_add[fn] = feature[fn]
-            # print(row_add[fn])
-        # print(row_add)
-        df = df.append(row_add, ignore_index=True)
+
+        if do_not_add:
+            continue
+        else:
+            # TODO: Replace append with more efficient way.
+            df = df.append(row_add, ignore_index=True)
+
+    QgsMessageLog.logMessage(message=f"Invalid Geometry Count: ({invalid_count})"
+                             , tag=f'{__name__}', level=Qgis.Info)
+
     # for fn in fieldnames:
     #     column_features = [feat[fn] for feat in features]
     #     appendage = {fn: column_features}
@@ -50,15 +78,27 @@ def df_to_gdf(df, coord_system):
     Converts pandas DataFrame to a GeoDataFrame along with setting the given coordinate system.
 
     :param df: Pandas DataFrame with QGIS vector layer data.
-    :type df: pandas.DataFrame
+    :type df: pd.DataFrame
     :param coord_system: Given coordinate system.
     :type coord_system: qgis.core.QgsCoordinateReferenceSystem
     :return: Converted GeoDataFrame
-    :rtype: geopandas.GeoDataFrame
+    :rtype: GeoDataFrame
     """
+    print(df, coord_system.toProj())
+    QgsMessageLog.logMessage(message=f"Converting coord_system ({coord_system}) to proj"
+                             , tag=f'{__name__}', level=Qgis.Info)
     proj_rep = coord_system.toProj()
-    gdf = gpd.GeoDataFrame(df)
+
+    # QgsMessageLog.logMessage(message=f"Converting df:\n{df.columns}\n{df.head()}\n{df}\n to GeoDataFrame"
+    #                          , tag=f'{__name__}', level=Qgis.Info)
+    gdf = gpd.GeoDataFrame(df, crs=proj_rep)
+
+    # QgsMessageLog.logMessage(message=f"Setting GeoDataFrame crs {proj_rep}"
+    #                          , tag=f'{__name__}', level=Qgis.Info)
     gdf.crs = proj_rep
+
+    # QgsMessageLog.logMessage(message=f"Returning GeoDataFrame"
+    #                          , tag=f'{__name__}', level=Qgis.Info)
     return gdf
 
 
@@ -69,10 +109,14 @@ def layer_to_gdf(layer):
     :param layer: QGIS vector layer
     :type layer: qgis.core.QgsVectorLayer
     :return: Converted GeoDataFrame
-    :rtype: geopandas.GeoDataFrame
+    :rtype: GeoDataFrame
     """
+    QgsMessageLog.logMessage(message=f"Converting QgsVectorLayer ({layer.name()}) to GeoDataFrame"
+                             , tag=f'{__name__}', level=Qgis.Info)
+
     df, coord_system = layer_to_df(layer)
-    return df_to_gdf(df, coord_system)
+    gdf = df_to_gdf(df, coord_system)
+    return gdf
 
 
 def plotting_directories(results_folder, name):
